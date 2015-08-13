140db8c0b4ac7342e71bc93b27aa50fd2f2b9019
Daniel R. Roe <daniel.r.roe@gmail.com>
2015-08-13 16:42:01 -0600

    Add single md with restraints test.

---
+++ test/Test_MD_RST/RunTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles run.000 ConstF.rst
+
+touch ConstF.rst
+
+OPTLINE="-i mdrst.opts -b 0 -e 0"
+RunTest "MD with restraints relative path test."
+DoTest ../mdsingle.groupfile.save run.000/groupfile
+DoTest mdrst.in.save run.000/md.in
+
+EndTest
--- test/Test_MD_Single/RunTest.sh
+++ test/Test_MD_Single/RunTest.sh
@@ -5,8 +5,8 @@
 CleanFiles run.000
 
 OPTLINE="-i md.opts -b 0 -e 0"
-RunTest "MD relative path test"
-DoTest mdsingle.groupfile.save run.000/groupfile
+RunTest "Single MD relative path test"
+DoTest ../mdsingle.groupfile.save run.000/groupfile
 DoTest mdsingle.in.save run.000/md.in
 
 EndTest
