ed9d41f8e41877343dfc213b665a75bccb50aadb
Daniel R. Roe <daniel.r.roe@gmail.com>
2015-08-13 16:13:13 -0600

    Make relative test input common

--- test/Test_MREMD_Relative/RunTest.sh
+++ test/Test_MREMD_Relative/RunTest.sh
@@ -4,26 +4,7 @@
 
 CleanFiles run.000 mremd.opts Hamiltonians.dat
 
-cat > mremd.opts <<EOF
-DIMENSION   ../Temperatures.dat
-DIMENSION   Hamiltonians.dat 
-DIMENSION   ../AmdDihedral.dat
-NSTLIM      500
-DT          0.002
-NUMEXCHG    100
-TEMPERATURE 300.0
-MDIN_FILE   ../pme.remd.gamma1.opts
-# Only fully archive lowest Hamiltonian
-FULLARCHIVE 0
-EOF
-
-cat > Hamiltonians.dat <<EOF
-#Hamiltonian
-../../AltDFC.01.PagF.TIP3P.ff14SB.parm7
-../../AltDFC.02.PagF.TIP3P.ff14SB.parm7
-EOF
-
-OPTLINE="-i mremd.opts -b 0 -e 0 -c ../../CRD"
+OPTLINE="-i ../relative.mremd.opts -b 0 -e 0 -c ../../CRD"
 RunTest "M-REMD relative path test."
 DoTest ../mremd.dim.save run.000/remd.dim
 DoTest relative.groupfile.save run.000/groupfile
