7b41ae96f0a6d7cb477e727ecdd059ed019e235b
Daniel R. Roe <daniel.r.roe@gmail.com>
2015-08-13 16:20:44 -0600

    Add archive input test.

---
+++ test/Test_Archive/RunTest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles run.000 Archive.0.0
+
+mkdir -p run.000/TRAJ
+touch run.000/TRAJ/rem.crd.001
+
+OPTLINE="-i ../relative.mremd.opts -b 0 -e 0 --archive --nocheck"
+RunTest "Archive input test."
+DoTest ar1.0.cpptraj.in.save Archive.0.0/ar1.0.cpptraj.in
+DoTest ar2.0.cpptraj.in.save Archive.0.0/ar2.0.cpptraj.in
+
+EndTest
