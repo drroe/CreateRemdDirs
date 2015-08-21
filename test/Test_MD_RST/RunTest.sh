fa9c4491c693d451e5e19551cd39726febd65f3b
Daniel R. Roe <Daniel.R.Roe@gmail.com>
2015-08-20 21:09:32 -0600

    DRR - Move test save to common location

--- test/Test_MD_RST/RunTest.sh
+++ test/Test_MD_RST/RunTest.sh
@@ -8,7 +8,8 @@ touch ConstF.rst
 
 OPTLINE="-i mdrst.opts -b 0 -e 0"
 RunTest "MD with restraints relative path test."
-DoTest ../mdsingle.groupfile.save run.000/groupfile
+#DoTest ../mdsingle.groupfile.save run.000/groupfile
 DoTest ../mdrst.in.save run.000/md.in
+DoTest ../RunMD.sh.save run.000/RunMD.sh
 
 EndTest
--- test/Test_MD_Single/RunTest.sh
+++ test/Test_MD_Single/RunTest.sh
@@ -7,7 +7,7 @@ CleanFiles run.000
 OPTLINE="-i md.opts -b 0 -e 0"
 RunTest "Single MD relative path test"
 #DoTest ../mdsingle.groupfile.save run.000/groupfile
-DoTest RunMD.sh.save run.000/RunMD.sh
+DoTest ../RunMD.sh.save run.000/RunMD.sh
 DoTest mdsingle.in.save run.000/md.in
 
 EndTest
