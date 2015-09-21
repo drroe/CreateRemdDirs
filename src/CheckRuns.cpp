60f9fc45d29db46306ffc559bd52701905060c71
Daniel R. Roe <daniel.r.roe@gmail.com>
2015-09-21 10:44:04 -0500

    DRR - Improvements to run checking. Report details of netcdf errors. When # of traj or restart files does not match # output files, try to locate where the difference is. Avoid repeating # frames warning during checkall when # actual frames does not change from replica to replica. Only print all OK if no warnings encountered.

--- src/CheckRuns.cpp
+++ src/CheckRuns.cpp
@@ -7,6 +7,43 @@
 #include "Messages.h"
 #include "TextFile.h"
 
+#ifdef HAS_NETCDF
+static inline int checkNCerr(int ncerr) {
+  if ( ncerr != NC_NOERR ) {
+    ErrorMsg("NETCDF: %s\n", nc_strerror(ncerr));
+    return 1;
+  }
+  return 0;
+}
+#endif
+
+static inline std::string Ext(std::string const& name) {
+  size_t found = name.find_last_of(".");
+  if (found == std::string::npos)
+    return std::string("");
+  return  name.substr(found);
+}
+
+/** Given two arrays of file names of different size try to determine
+  * where they differ based on extension.
+  */
+static void CompareStrArray(StrArray const& a1, StrArray const& a2) {
+  unsigned int max = (unsigned int)std::min(a1.size(), a2.size());
+  unsigned int idx = 0;
+  for (idx = 0; idx != max; idx++) {
+    std::string e1 = Ext(a1[idx]);
+    std::string e2 = Ext(a2[idx]);
+    if (e1 != e2) {
+      ErrorMsg("Differs at '%s', '%s'\n", a1[idx].c_str(), a2[idx].c_str());
+      return;
+    }
+  }
+  if (idx < a1.size())
+    ErrorMsg("Differs at '%s'\n", a1[idx].c_str());
+  if (idx < a2.size())
+    ErrorMsg("Differs at '%s'\n", a2[idx].c_str());
+}
+
 int CheckRuns(std::string const& TopDir, StrArray const& RunDirs, bool firstOnly) {
 #ifdef HAS_NETCDF
   if (firstOnly)
@@ -14,6 +51,7 @@ int CheckRuns(std::string const& TopDir, StrArray const& RunDirs, bool firstOnly
   else
     Msg("Checking all output/traj for all runs.\n");
   int debug = 0;
+  int Nwarnings = 0;
   for (StrArray::const_iterator rdir = RunDirs.begin(); rdir != RunDirs.end(); ++rdir) {
     if (ChangeDir( TopDir )) return 1;
     if (!fileExists( *rdir ))
@@ -42,9 +80,12 @@ int CheckRuns(std::string const& TopDir, StrArray const& RunDirs, bool firstOnly
       if (traj_files.size() != output_files.size()) {
         ErrorMsg("Number of output files %zu != # of traj files %zu.\n",
                  output_files.size(), traj_files.size());
+        CompareStrArray( output_files, traj_files );
         return 1;
       }
       // Loop over output and trajectory files.
+      int badFrameCount = -1;
+      int numBadFrameCount = 0;
       bool check_restarts = false;
       StrArray::const_iterator tname = traj_files.begin();
       for (StrArray::const_iterator fname = output_files.begin();
@@ -97,24 +138,31 @@ int CheckRuns(std::string const& TopDir, StrArray const& RunDirs, bool firstOnly
         }
         // Get actual number of frames from NetCDF file.
         int ncid = -1;
-        if ( nc_open(tname->c_str(), NC_NOWRITE, &ncid) != NC_NOERR ) return 1;
+        if ( checkNCerr(nc_open(tname->c_str(), NC_NOWRITE, &ncid)) ) return 1;
         int dimID;
         size_t slength = 0;
-        if ( nc_inq_dimid(ncid, "frame", &dimID) != NC_NOERR ) return 1;
-        if ( nc_inq_dimlen(ncid, dimID, &slength) != NC_NOERR ) return 1;
+        if ( checkNCerr(nc_inq_dimid(ncid, "frame", &dimID))  ) return 1;
+        if ( checkNCerr(nc_inq_dimlen(ncid, dimID, &slength)) ) return 1;
         int actualFrames = (int)slength;
         nc_close( ncid );
         if (debug > 0) Msg("\tActual Frames: %i\n", actualFrames);
         // If run did not complete, check restart files if replica.
         if (expectedFrames != actualFrames) {
-          Msg("Warning: # actual frames %i != # expected frames.\n",
-              actualFrames, expectedFrames);
-          if (!is_md) check_restarts = true; 
+          ++numBadFrameCount;
+          ++Nwarnings;
+          if (badFrameCount != actualFrames) { // To avoid repeated checkall warnings
+            Msg("Warning: # actual frames %i != # expected frames %i.\n",
+                actualFrames, expectedFrames);
+            badFrameCount = actualFrames;
+          }
+          if (!is_md) check_restarts = true;
         } else {
           if (debug > 0) Msg("\tOK.\n");
         }
         if (firstOnly) break;
       } // END loop over output files for run
+      if (numBadFrameCount > 0)
+        Msg("Warning: Frame count did not match for %i replicas.\n", numBadFrameCount);
       if (check_restarts) {
         StrArray restart_files = ExpandToFilenames("RST/*.rst7");
         if (restart_files.empty())
@@ -122,6 +170,7 @@ int CheckRuns(std::string const& TopDir, StrArray const& RunDirs, bool firstOnly
         if (restart_files.size() != output_files.size()) {
           ErrorMsg("Number of restart files %zu != # output files %zu\n",
                    restart_files.size(), output_files.size());
+          CompareStrArray( restart_files, output_files );
           return 1;
         }
         double rst_time0 = 0.0;
@@ -130,9 +179,9 @@ int CheckRuns(std::string const& TopDir, StrArray const& RunDirs, bool firstOnly
         {
           int ncid = -1, timeVID = -1;
           double rsttime = -1.0;
-          if ( nc_open(rfile->c_str(), NC_NOWRITE, &ncid) != NC_NOERR ) return 1; // TODO Ascii
-          if ( nc_inq_varid(ncid, "time", &timeVID) != NC_NOERR ) return 1;
-          if ( nc_get_var_double(ncid, timeVID, &rsttime) != NC_NOERR ) return 1;
+          if ( checkNCerr(nc_open(rfile->c_str(), NC_NOWRITE, &ncid)) ) return 1; // TODO Ascii
+          if ( checkNCerr(nc_inq_varid(ncid, "time", &timeVID)      ) ) return 1;
+          if ( checkNCerr(nc_get_var_double(ncid, timeVID, &rsttime)) ) return 1;
           if (rfile == restart_files.begin()) {
             rst_time0 = rsttime;
             Msg("\tInitial restart time: %g\n", rst_time0);
@@ -145,7 +194,10 @@ int CheckRuns(std::string const& TopDir, StrArray const& RunDirs, bool firstOnly
       }
     }
   } // END loop over runs
-  Msg("\tRun OK.\n");
+  if (Nwarnings == 0)
+    Msg("  All checks OK.\n");
+  else
+    Msg("  Runs seem OK, but some warnings were encountered.\n");
 # else
   Msg("Warning: Compiled without NetCDF. Checking of runs is disabled.\n");
 # endif
