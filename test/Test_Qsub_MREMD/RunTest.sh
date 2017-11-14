2096419cfde948aa557c71bde295c1fb24fb69bf
Daniel R. Roe <daniel.roe@nih.gov>
2017-11-14 13:10:14 -0500

    DRR - Finish fixing for absolute path

--- test/Test_Qsub_MREMD/RunTest.sh
+++ test/Test_Qsub_MREMD/RunTest.sh
@@ -3,7 +3,11 @@
 . ../MasterTest.sh
 
 CleanFiles run.00? mremd.opts Hamiltonians.dat qsub.opts analyze.opts Analyze.0.1 \
-           Archive.0.1 archive.sbatch.0.1.sh RunArchive.0.1.sh 
+           Archive.0.1 archive.sbatch.0.1.sh RunArchive.0.1.sh \
+           run1.sbatch.sh run1.sbatch.sh.save \
+           run0.qsub.sh run0.qsub.sh.save \
+           analyze.sbatch.sh analyze.sbatch.sh.save \
+           archive.sbatch.0.1.sh archive.sbatch.0.1.sh.save
 
 if [ -z "$AMBERHOME" ] ; then
   echo "Warning: Skipping submission test."
@@ -33,6 +37,7 @@ RunTest "MREMD job submission test (SBATCH)."
 DoTest ../mremd.dim.save run.000/remd.dim
 #DoTest relative.groupfile.save run.000/groupfile
 DoTest ../in.001.save run.000/INPUT/in.001
+sed "s:amberhome:$AMBERHOME:g" run1.sbatch.sh.template > run1.sbatch.sh.save
 DoTest run1.sbatch.sh.save run.001/sbatch.sh
 
 MakeOpts PBS
@@ -58,7 +63,9 @@ OPTLINE="-i ../relative.mremd.opts --analyze --archive -b 0 -e 1 -s --nocheck -t
 RunTest "MREMD analysis/archive job submission test (PBS)"
 DoTest RunAnalysis.sh.save Analyze.0.1/RunAnalysis.sh
 DoTest batch.cpptraj.in.save Analyze.0.1/batch.cpptraj.in
+sed "s:amberhome:$AMBERHOME:g" analyze.sbatch.sh.template > analyze.sbatch.sh.save
 DoTest analyze.sbatch.sh.save Analyze.0.1/sbatch.sh
+sed "s:amberhome:$AMBERHOME:g" archive.sbatch.0.1.sh.template > archive.sbatch.0.1.sh.save
 DoTest archive.sbatch.0.1.sh.save archive.sbatch.0.1.sh
 DoTest RunArchive.0.1.sh.save RunArchive.0.1.sh
 
