1cba4610ad1a3d0268aa52139fa29749051aa5be
Daniel R. Roe <daniel.roe@nih.gov>
2018-08-24 12:22:21 -0400

    MD reference test

--- test/Test_MD_RST/RunTest.sh
+++ test/Test_MD_RST/RunTest.sh
@@ -10,6 +10,6 @@ OPTLINE="-i mdrst.opts -b 0 -e 0"
 RunTest "MD with restraints relative path test."
 #DoTest ../mdsingle.groupfile.save run.000/groupfile
 DoTest ../mdrst.in.save run.000/md.in
-DoTest ../RunMD.sh.save run.000/RunMD.sh
+DoTest RunMD.sh.save run.000/RunMD.sh
 
 EndTest
