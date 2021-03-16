9ad72e92340bd707173625949026c4869524d109
Daniel R. Roe <daniel.roe@nih.gov>
2021-03-16 16:04:25 -0400

    Minor version bump for single remd coords ability

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "FileRoutines.h"
 #include "StringRoutines.h"
 
-static const char* VERSION = "1.01";
+static const char* VERSION = "1.02";
 
 static void CmdLineHelp() {
   Msg("Command line options:\n"
