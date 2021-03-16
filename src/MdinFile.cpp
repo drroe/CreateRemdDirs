6ba4badfeba74b0df7d912d858e4f241f0760acc
Daniel R. Roe <daniel.roe@nih.gov>
2021-03-16 09:29:00 -0400

    Start adding token parsing

--- src/MdinFile.cpp
+++ src/MdinFile.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include "MdinFile.h"
 #include "TextFile.h"
 #include "Messages.h"
@@ -5,6 +6,41 @@
 
 /** CONSTRUCTOR */
 MdinFile::MdinFile() {}
+
+MdinFile::TokenArray MdinFile::TokenizeLine(std::string const& inputString) {
+  static const char* separator = ",";
+  TokenArray Tokens;
+  if (inputString.empty()) 
+    return Tokens;
+
+  typedef std::vector<std::string> Sarray;
+  Sarray Pairs;
+
+  // Copy inputString to temp since it is destroyed by tokenize.
+  size_t inputStringSize = inputString.size();
+  if (inputStringSize < 1) return Tokens;
+  char* tempString = new char[ inputStringSize+1 ];
+  inputString.copy( tempString, inputStringSize, 0 );
+  tempString[ inputStringSize ] = '\0'; // copy() does not append null 
+  // Remove newline char from tempString if present
+  if ( tempString[ inputStringSize - 1 ] == '\n' )
+    tempString[ inputStringSize - 1 ] = '\0';
+
+  // Begin tokenization
+  char* pch = strtok(tempString, separator);
+  if (pch != 0) {
+    while (pch != 0) {
+      Pairs.push_back( std::string(pch) );
+      pch = strtok(0, separator);
+    }
+  }
+
+  Msg("DEBUG: Tokens:\n");
+  for (Sarray::const_iterator it = Pairs.begin(); it != Pairs.end(); ++it)
+    Msg("\t%s\n", it->c_str());
+
+  return Tokens;
+}
 
 /** Parse input from MDIN file. */
 int MdinFile::ParseFile(std::string const& fname) {
--- src/MdinFile.h
+++ src/MdinFile.h
@@ -1,11 +1,19 @@
 #ifndef INC_MDIN_FILE
 #define INC_MDIN_FILE
 #include <string>
+#include <vector>
 /// Used to parse input from an Amber MDIN file
 class MdinFile {
   public:
     MdinFile();
 
     int ParseFile(std::string const&);
+  private:
+    /// Pair namelist variable with value
+    typedef std::pair<std::string, std::string> TokenType;
+    /// Array of namelist variable/value pairs
+    typedef std::vector<TokenType> TokenArray;
+
+    static TokenArray TokenizeLine(std::string const&);
 };
 #endif
