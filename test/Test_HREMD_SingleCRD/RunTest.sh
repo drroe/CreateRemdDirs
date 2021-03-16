ad17ee31fe797d25861b6bb216dc9a3d1ae55666
Daniel R. Roe <daniel.roe@nih.gov>
2021-03-16 16:01:49 -0400

    Save the groupfile

--- test/Test_HREMD_SingleCRD/RunTest.sh
+++ test/Test_HREMD_SingleCRD/RunTest.sh
@@ -4,10 +4,10 @@
 
 CleanFiles run.000 mremd.opts Hamiltonians.dat
 
-OPTLINE="-i hremd.opts -b 0 -e 0 -c ../../CRD"
-RunTest "H-REMD relative path test."
+OPTLINE="-i hremd.opts -b 0 -e 0"
+RunTest "H-REMD single start coords test"
 #DoTest ../mremd.dim.save run.000/remd.dim
 DoTest groupfile.save run.000/groupfile
-DoTest in.001.save run.000/INPUT/in.001
+#DoTest in.001.save run.000/INPUT/in.001
 
 EndTest
