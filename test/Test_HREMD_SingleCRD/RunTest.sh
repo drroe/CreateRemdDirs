e99afbc76448418407f8b232ae8ae3909f25150e
Daniel R. Roe <daniel.roe@nih.gov>
2021-03-16 16:00:16 -0400

    Start adding test for single coords

---
+++ test/Test_HREMD_SingleCRD/RunTest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. ../MasterTest.sh
+
+CleanFiles run.000 mremd.opts Hamiltonians.dat
+
+OPTLINE="-i hremd.opts -b 0 -e 0 -c ../../CRD"
+RunTest "H-REMD relative path test."
+#DoTest ../mremd.dim.save run.000/remd.dim
+DoTest groupfile.save run.000/groupfile
+DoTest in.001.save run.000/INPUT/in.001
+
+EndTest
