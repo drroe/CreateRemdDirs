75d371a02ebe76017bafe7bf662d8bd70966baff
Daniel R. Roe <daniel.roe@nih.gov>
2021-03-18 09:55:49 -0400

    Start adding a manager class.

---
+++ src/Manager.cpp
@@ -0,0 +1,35 @@
+#include "Manager.h"
+#include "Messages.h"
+#include "TextFile.h"
+
+Manager::Manager() {}
+
+/** Initialize with input file. */
+int Manager::InitManager(std::string const& inputFileName) {
+  if (inputFileName.empty()) {
+    ErrorMsg("No manager input file given.\n");
+    return 1;
+  }
+
+  TextFile input;
+  if (input.OpenRead(inputFileName)) {
+    ErrorMsg("Could not open manager input file '%s'\n", inputFileName.c_str());
+    return 1;
+  }
+  static const char* SEP = " \n\r";
+  int ncols = input.GetColumns(SEP);
+  while (ncols > -1) {
+    if (ncols > 0) {
+      if (input.Token(0)[0] != '#') {
+        // Expect <system dir>, <description>
+        if (ncols > 1) {
+          Msg("System: %s  Description: '%s'\n", input.Token(0).c_str(), input.Token(1).c_str());
+        }
+      }
+    }
+    ncols = input.GetColumns(SEP);
+  }
+  input.Close();
+
+  return 0;
+}
---
+++ src/Manager.h
@@ -0,0 +1,12 @@
+#ifndef INC_MANAGER_H
+#define INC_MANAGER_H
+#include <string>
+/// Class to manage runs
+class Manager {
+  public:
+    Manager();
+
+    int InitManager(std::string const&);
+  private:
+};
+#endif
