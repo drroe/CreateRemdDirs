58989c6074bbef714a543f75c1a224d5837304bd
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-01-18 15:02:35 -0500

    Add more functions for accessing options

--- src/MdOptions.h
+++ src/MdOptions.h
@@ -6,7 +6,17 @@ class MdOptions {
   public:
     MdOptions();
 
-    Option<double> const& TimeStep() const { return timeStep_; }
+    Option<int>&    Set_N_Steps()      return { nsteps_; }
+    Option<int>&    Set_RandomSeed()   return { random_seed_; }
+    Option<int>&    Set_N_Exchanges()  return { nexchanges_; }
+    Option<double>& Set_TimeStep()     return { timeStep_; }
+    Option<double>& Set_Temperature0() return { temp0_; }
+
+    Option<int>    const& N_Steps()      const { return nsteps_; }
+    Option<int>    const& RandomSeed()   const { return random_seed_; }
+    Option<int>    const& N_Exchanges()  const { return nexchages_; }
+    Option<double> const& TimeStep()     const { return timeStep_; }
+    Option<double> const& Temperature0() const { return temp0_; }
   private:
     Option<int> nsteps_;      ///< Number of simulation steps.
     Option<int> random_seed_; ///< Simulation random number generator seed.
