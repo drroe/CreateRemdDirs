f531ff57e39a305cfc41bfa93fcc595b72f36eca
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-01-24 15:32:08 -0500

    Add run creation routine

--- src/Run.cpp
+++ src/Run.cpp
@@ -2,6 +2,7 @@
 #include "Messages.h"
 #include "FileRoutines.h"
 #include "StringRoutines.h"
+#include "MdPackage.h"
 
 using namespace Messages;
 
@@ -11,13 +12,35 @@ Run::Run() :
   debug_(0)
 {}
 
-/** Setup existing run dir. Assumes we are in that dir. */
+/** Setup existing run dir (no changes). Assumes we are in that dir. FIXME should it?*/
 int Run::SetupExisting(std::string const& runDir)
 {
   using namespace FileRoutines;
   rundir_ = runDir;
-  setupDir_ = GetWorkingDir();
   idx_ = StringRoutines::convertToInteger( Extension(runDir) );
+  setupDir_ = GetWorkingDir();
   Msg("DEBUG: Run::SetupExisting: '%s' '%s' '%i'\n", rundir_.c_str(), setupDir_.c_str(), idx_);
   return 0;
+}
+
+/** Create new or overwrite existing run dir. */
+int Run::CreateNew(std::string const& runDir, Creator const& creator, MdPackage* mdpackage,
+                   int start_run, int run_num, std::string const& prevDir)
+{
+  using namespace FileRoutines;
+  rundir_ = runDir;
+  idx_ = StringRoutines::convertToInteger( Extension(runDir) );
+  if (!fileExists(rundir_)) {
+    // Create
+    if (Mkdir(rundir_)) return 1;
+  }
+  if (ChangeDir(rundir_)) return 1;
+  setupDir_ = GetWorkingDir();
+
+  return mdpackage->CreateInputFiles(creator, start_run, run_num, rundir_, prevDir);
+}
+
+/** Print run info to stdout. */
+void Run::RunInfo() const {
+  Msg("Placeholder.\n"); //FIXME
 }
--- src/Run.h
+++ src/Run.h
@@ -1,6 +1,8 @@
 #ifndef INC_RUN_H
 #define INC_RUN_H
 #include <string>
+class Creator;
+class MdPackage;
 /// Hold information for a single run
 class Run {
   public:
@@ -10,10 +12,14 @@ class Run {
     int Debug() const { return debug_; }
     /// \return Run index
     int RunIndex() const { return idx_; }
+    /// Print run info to stdout
+    void RunInfo() const;
     /// \return run dir name
     std::string const& RunDirName() const { return rundir_; }
     /// Setup existing directory - run dir name
     int SetupExisting(std::string const&);
+    /// Create new directory - rundir, creator, mdpackage, start #, run #, prev run dir
+    int CreateNew(std::string const&, Creator const&, MdPackage*, int, int, std::string const&);
     
   private:
     std::string rundir_;   ///< Run directory. May be relative.
