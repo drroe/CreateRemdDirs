a483808fccebd21bc3628be73abac26d4e0e7d5d
Daniel R. Roe <daniel.roe@nih.gov>
2023-01-31 15:13:45 -0500

    Move Job stuff to Submitter

--- src/Submitter.h
+++ src/Submitter.h
@@ -1,11 +1,24 @@
 #ifndef INC_SUBMITTER_H
 #define INC_SUBMITTER_H
-/// Used to submit jobs
+#include <vector>
+/// Used to submit jobs for runs in a particular system
 class Submitter {
   public:
+    /// Type describing how to handle dependencies
+    enum DependType { BATCH = 0, SUBMIT, NONE, NO_DEPENDS };
+    /// CONSTRUCTOR
     Submitter();
     /// Read options from a file
     int ReadOptions(std::string const&);
   private:
+    std::string job_name_;  ///< Job name
+    int nodes_;             ///< Number of nodes required
+    int procs_;             ///< Number of processors required
+    std::string walltime_;  ///< Wallclock time required
+    std::string email_;     ///< User email address
+    std::string account_;   ///< Account for running jobs
+    std::string program_;   ///< Executable name
+    std::string mpirun_;    ///< MPI run command if neededi
+    DependType dependType_; ///< Describes how to handle job dependncies
 };
 #endif
