cea55304a348a4b691de3fc19db3e8839f89e02b
Daniel R. Roe <daniel.r.roe@gmail.com>
2023-02-10 08:51:40 -0500

    Change default behavior of list to show only active project/system to be consistent with how all other commands operate.

--- src/Exec_List.cpp
+++ src/Exec_List.cpp
@@ -9,14 +9,73 @@ using namespace Messages;
 Exec_List::Exec_List() {}
 
 void Exec_List::Help() const {
-  Msg("\t[{[project <idx>] [system <idx>] | running | active}]\n"
-      "  List projects/systems. If 'running' specified only list jobs running in queue.\n"
-      "  If 'active' specified only list runs from active project/system.\n");
+  Msg("\t[{proj[ects] | [project <idx>] [system <idx>] [all]| running}]\n"
+      "  List active project/system. If 'proj' or 'projects' is specifed, list\n"
+      "  all projects. If 'project' and/or 'system' specified, show only that\n"
+      "  project/system; the 'all' keyword can be used to show all systems or\n"
+      "  runs. If 'running' specified only list jobs running in queue.\n"
+     );
 }
 
 /** List all systems. */
 Exec::RetType Exec_List::Execute(Manager& manager, Cols& args) const {
+  enum ListModeType { ACTIVE = 0, RUNNING, PROJECTS };
+  // Process input
+  ListModeType listMode = ACTIVE;
+  // Check for running mode args
   if (args.HasKey("running")) {
+    listMode = RUNNING;
+  }
+  // Check for projects mode args
+  static const int SHOW_ALL = -1;
+  static const int HIDE_ALL = -2;
+  int tgtProjectIdx = HIDE_ALL;
+  int tgtSystemIdx = HIDE_ALL;
+  int tgtRunIdx = HIDE_ALL;
+  if (args.HasKey("projects") || args.HasKey("proj")) {
+    listMode = PROJECTS;
+    tgtProjectIdx = SHOW_ALL;
+  }
+  // Project
+  if (args.GetKeyInteger(tgtProjectIdx, "project", -1)) return ERR;
+  // If a specific project was chosen, list all systems by default.
+  if (tgtProjectIdx > -1) {
+    listMode = PROJECTS;
+    tgtSystemIdx = SHOW_ALL;
+  } else if (manager.Projects().size() == 1)
+    tgtSystemIdx = SHOW_ALL;
+  // System
+  if (args.GetKeyInteger(tgtSystemIdx, "system", tgtSystemIdx)) return ERR;
+  // If a specific system was chosen, list all runs by default.
+  if (tgtSystemIdx > -1) {
+    listMode = PROJECTS;
+    tgtRunIdx = SHOW_ALL;
+    // If no project specified, choose the active project
+    if (tgtProjectIdx < 0)
+      tgtProjectIdx = manager.ActiveProjectIdx();
+  }
+  // If there is only 1 project and 1 system, list all runs by default.
+  if (manager.Projects().size() == 1 && manager.Projects()[0].Systems().size() == 1)
+    tgtRunIdx = SHOW_ALL;
+  // If a project has been chosen and 1 system, list all runs by default.
+  if (tgtProjectIdx > -1) {
+    if (manager.Projects()[tgtProjectIdx].Systems().size() == 1)
+      tgtRunIdx = SHOW_ALL;
+  }
+  // 'all' overrides everything else that is not already set to a specific index
+  if (args.HasKey("all")) {
+    listMode = PROJECTS;
+    if (tgtProjectIdx < 0)
+      tgtProjectIdx = SHOW_ALL;
+    if (tgtSystemIdx < 0)
+      tgtSystemIdx = SHOW_ALL;
+    if (tgtRunIdx < 0)
+      tgtRunIdx = SHOW_ALL;
+  }
+
+  // -----------------------------------
+  if (listMode == RUNNING) {
+    Msg("Running jobs:\n");
     // List only running jobs
     int pidx = 0;
     for (Manager::ProjectArray::const_iterator project = manager.Projects().begin();
@@ -49,7 +108,8 @@ Exec::RetType Exec_List::Execute(Manager& manager, Cols& args) const {
   }
 
   // -----------------------------------
-  if (args.HasKey("active")) {
+  if (listMode == ACTIVE) {
+    Msg("Active project/system:\n");
     if (manager.HasActiveProjectSystem()) {
       Project& activeProject = manager.ActiveProject();
       System& activeSystem = manager.ActiveProjectSystem();
@@ -82,46 +142,7 @@ Exec::RetType Exec_List::Execute(Manager& manager, Cols& args) const {
   }
 
   // -----------------------------------
-  static const int SHOW_ALL = -1;
-  static const int HIDE_ALL = -2;
-  int tgtProjectIdx = SHOW_ALL;
-  int tgtSystemIdx = HIDE_ALL;
-  int tgtRunIdx = HIDE_ALL;
-
-  if (args.GetKeyInteger(tgtProjectIdx, "project", -1)) return ERR;
-  // If a specific project was chosen, list all systems by default.
-  if (tgtProjectIdx > -1)
-    tgtSystemIdx = SHOW_ALL;
-  else if (manager.Projects().size() == 1)
-    tgtSystemIdx = SHOW_ALL;
-
-  if (args.GetKeyInteger(tgtSystemIdx, "system", tgtSystemIdx)) return ERR;
-  // If a specific system was chosen, list all runs by default.
-  if (tgtSystemIdx > -1) {
-    tgtRunIdx = SHOW_ALL;
-    // If no project specified, choose the active project
-    if (tgtProjectIdx < 0)
-      tgtProjectIdx = manager.ActiveProjectIdx();
-  }
-
-  // If there is only 1 project and 1 system, list all runs by default.
-  if (manager.Projects().size() == 1 && manager.Projects()[0].Systems().size() == 1)
-    tgtRunIdx = SHOW_ALL;
-  // If a project has been chosen and 1 system, list all runs by default.
-  if (tgtProjectIdx > -1) {
-    if (manager.Projects()[tgtProjectIdx].Systems().size() == 1)
-      tgtRunIdx = SHOW_ALL;
-  }
-
-  // 'all' overrides everything else that is not already set to a specific index
-  if (args.HasKey("all")) {
-    if (tgtProjectIdx < 0)
-      tgtProjectIdx = SHOW_ALL;
-    if (tgtSystemIdx < 0)
-      tgtSystemIdx = SHOW_ALL;
-    if (tgtRunIdx < 0)
-      tgtRunIdx = SHOW_ALL;
-  }
+  // listMode_ == PROJECTS
 
   Msg("Manager top directory: %s\n", manager.topDirName());
 
